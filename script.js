1cef6e9714197d56cca9063128abf21cac44c4d6
letsinnovate-code <ap1106561@gmail.com>
2024-07-26 17:05:44 +0530

    new changes

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
 
+
+const searchBtn = document.querySelector(".search");
+const inputValue = document.querySelector(".city_name");
+const API_KEY = `ee776f55501df664931d5a5629d4ef09`;
+
+function getWeatherDetails (name ,lat , lon){
+
+   
+    const url = `https://easy-weather1.p.rapidapi.com/daily/5?latitude=${lat}&longitude=${lon}`;
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': 'ffca0c2181mshc8d532270606f5cp1b7334jsn3ee9b294e7a3',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'x-rapidapi-host': 'easy-weather1.p.rapidapi.com'
 	}
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi',options)
-.then(response=>response.json() )
-.then(result=>console.log(result))
-.catch(err=>console.log(err));
+
+fetch(url, options)
+.then(response=>response.json())
+.then(data=>{
+    console.log(data)
+
+    data.
+
+
+})
+.catch(err=>console.log(err))
+
+}
+
+
+
+    function getCity(){
+        const cityName = inputValue.value.trim();
+        if(!cityName) return;
+        console.log(cityName);
+
+       
+        
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`,)
+           .then((response) => response.json())
+           .then((data) => { console.log(data)
+            if(data.length == 0) return alert(`no cordinates found for ${cityName}`);
+               
+                const {name , lat ,lon}=data[0]
+            getWeatherDetails(name, lat , lon);
+           })
+           .catch((error) => console.log(error));
+       
+    }
+
+    searchBtn.addEventListener("click",getCity);
+
+
+
+    
+
+
+
+
