7c2595d911be1e3b70eab470a0cc7c5792e8f956
letsinnovate-code <ap1106561@gmail.com>
2024-07-27 03:30:58 +0530

    new changes

--- script.js
+++ script.js
@@ -1,62 +1,81 @@
-
-
 const searchBtn = document.querySelector(".search");
 const inputValue = document.querySelector(".city_name");
+const weatherData = document.querySelector(".weather_data");
+const weatherDataFirst = document.querySelector(".weather_data_first");
 const API_KEY = `ee776f55501df664931d5a5629d4ef09`;
 
-function getWeatherDetails (name ,lat , lon){
-
-   
-    const url = `https://easy-weather1.p.rapidapi.com/daily/5?latitude=${lat}&longitude=${lon}`;
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'ffca0c2181mshc8d532270606f5cp1b7334jsn3ee9b294e7a3',
-		'x-rapidapi-host': 'easy-weather1.p.rapidapi.com'
-	}
-};
-
-
-fetch(url, options)
-.then(response=>response.json())
-.then(data=>{
-    console.log(data)
-
-    data.
-
-
-})
-.catch(err=>console.log(err))
-
+function firstweatherCard(fiveDaysForecast ,data){
+    return `   <li class="current_day p-7 text-lg">
+            <h1 class="text-white">${data.city.name} (${fiveDaysForecast.dt_txt.split(" ")[0]})</h1>
+            <h2 class="text-white">Temp: ${fiveDaysForecast.main.temp}C</h2>
+            <h2 class="text-white">Wind: ${fiveDaysForecast.wind.speed}m/s</h2>
+            <h2 class="text-white">Humidity:${fiveDaysForecast.main.humidity}%</h2>
+          </li>
+           <li>
+            <img src="https://openweathermap.org/img/wn/${fiveDaysForecast.weather[0].icon}@2x.png" , alt="weather_icon" alt="" />
+          </li>`;
 }
 
+function weatherCard(weatherItem) {
+  return `<li class="card rounded-lg flex flex-col justify-center w-52 p-6 h-64">
+              <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+              <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" class="h-3 w-3" , alt="weather_icon">
+              <h4>Temp: ${weatherItem.main.temp}C</h4>
+              <h4>Wind: ${weatherItem.wind.speed}m/s</h4>
+              <h4>Humidity:${weatherItem.main.humidity}%</h4>
+            </li>`;
+}
 
+function getWeatherDetails(name, lat, lon) {
 
-    function getCity(){
-        const cityName = inputValue.value.trim();
-        if(!cityName) return;
-        console.log(cityName);
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
-       
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const uniqueforecastDays = [];
+      const fiveDaysForecast = data.list.filter((forecast) => {
+        const forecastDate = new Date(forecast.dt_txt).getDate();
+        if (!uniqueforecastDays.includes(forecastDate)) {
+          return uniqueforecastDays.push(forecastDate);
+        }
+      });
+      
+      inputValue.value = "";
+      weatherData.innerHTML = "";
+      fiveDaysForecast.forEach((weatherItem) => {
+        weatherData.insertAdjacentHTML(
+          "beforeend",
+          weatherCard(weatherItem)
+        );
         
-        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`,)
-           .then((response) => response.json())
-           .then((data) => { console.log(data)
-            if(data.length == 0) return alert(`no cordinates found for ${cityName}`);
-               
-                const {name , lat ,lon}=data[0]
-            getWeatherDetails(name, lat , lon);
-           })
-           .catch((error) => console.log(error));
-       
-    }
+        
+      });
+      weatherDataFirst.innerHTML="";
+      weatherDataFirst.insertAdjacentHTML("beforeend",firstweatherCard(fiveDaysForecast[0],data));
+    })
+    .catch((err) =>{console.log(err)});
+}
 
-    searchBtn.addEventListener("click",getCity);
+function getCity() {
+  const cityName = inputValue.value.trim();
+  if (!cityName) return;
+  console.log(cityName);
 
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.length == 0) return alert(`no cordinates found for ${cityName}`);
 
+      const { name, lat, lon } = data[0];
+      getWeatherDetails(name, lat, lon);
+      
+    })
+    .catch((error) => console.log(error));
+}
 
-    
-
-
-
-
+searchBtn.addEventListener("click", getCity);
