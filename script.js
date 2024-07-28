32276a7d8fa8d135f4a2598b9f80643c356708cb
letsinnovate-code <ap1106561@gmail.com>
2024-07-28 23:57:36 +0530

    new changes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const searchBtn = document.querySelector(".search");
 const inputValue = document.querySelector(".city_name");
-const weatherData = document.querySelector(".weather_data");
+let weatherData = document.querySelector(".weather_data");
 const weatherDataFirst = document.querySelector(".weather_data_first");
 const API_KEY = `ee776f55501df664931d5a5629d4ef09`;
+
+
+// function to display current day weather details 
 
 function firstweatherCard(fiveDaysForecast ,data){
     return `   <li class="current_day p-7 text-lg">
@@ -16,6 +19,8 @@ function firstweatherCard(fiveDaysForecast ,data){
           </li>`;
 }
 
+//  function to display five days weather details
+
 function weatherCard(weatherItem) {
   return `<li class="card rounded-lg flex flex-col justify-center w-52 p-6 h-64">
               <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
@@ -26,6 +31,8 @@ function weatherCard(weatherItem) {
             </li>`;
 }
 
+// function to get the weather  details from the server using latitude and longitude 
+
 function getWeatherDetails(name, lat, lon) {
 
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
@@ -34,13 +41,17 @@ function getWeatherDetails(name, lat, lon) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      
       const uniqueforecastDays = [];
       const fiveDaysForecast = data.list.filter((forecast) => {
         const forecastDate = new Date(forecast.dt_txt).getDate();
         if (!uniqueforecastDays.includes(forecastDate)) {
-          return uniqueforecastDays.push(forecastDate);
+          uniqueforecastDays.push(forecastDate);
+          return true;
         }
+        return false;
       });
+      localStorage.setItem("weatherData",JSON.stringify(fiveDaysForecast));
       
       inputValue.value = "";
       weatherData.innerHTML = "";
@@ -52,11 +63,33 @@ function getWeatherDetails(name, lat, lon) {
         
         
       });
+      
       weatherDataFirst.innerHTML="";
       weatherDataFirst.insertAdjacentHTML("beforeend",firstweatherCard(fiveDaysForecast[0],data));
     })
     .catch((err) =>{console.log(err)});
 }
+
+document.addEventListener("DOMContentLoaded",()=>{
+  let savedWeatherData = JSON.parse(localStorage.getItem("weatherData"));
+
+  if(savedWeatherData){
+    
+    weatherData = "";
+    savedWeatherData.forEach((weatherItem)=>{
+
+      weatherData.insertAdjacentHTML("beforeend",weatherCard(weatherItem));
+
+    });
+    if(savedWeatherData.length > 0){
+      weatherDataFirst.innerHTML = "";
+      weatherDataFirst.insertAdjacentHTML("beforeend",firstweatherCard(savedWeatherData[0],{list:savedWeatherData}));
+    }
+  }
+});
+
+// function to get the details of longitude and latitude by search the city name 
+
 
 function getCity() {
   const cityName = inputValue.value.trim();
@@ -78,4 +111,8 @@ function getCity() {
     .catch((error) => console.log(error));
 }
 
+// add the event listener to the search button 
+
 searchBtn.addEventListener("click", getCity);
+
+// add the event listener to the document 
